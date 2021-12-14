dba3f6af3baa4f24fdd31047569a660b89061273
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2021-12-14 13:14:53 +1100

    Stage 4/5

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,44 +1,46 @@
-game_size = 3
+class TicTacToe:
+    def __init__(self, game_size=3):
+        self.game_size = game_size
+        self.header_len = '-' * (game_size * 3)
+        self.game_progress = [x for x in input('Enter cells:')]
+        self.winnerX = ''.join('X' for x in range(self.game_size))
+        self.winnerO = ''.join('O' for x in range(self.game_size))
 
-winnerX = ''.join('X' for x in range(game_size))
-winnerO = ''.join('O' for x in range(game_size))
+    def index_by_coords(self, row, col):
+        if row == 2:
+            return row + col
+        return row**2 - (row - col + 1)
 
-header = footer = '-' * (game_size * 3)
-game_progress = input()
+    def get_board(self):
+        print(self.header_len)
+        for i in range(0, pow(self.game_size, 2), self.game_size):
+            print('|', *self.game_progress[i: i + self.game_size], '|')
+        print(self.header_len)
 
-ends_of_game = ['Impossible', 'X wins', 'O wins', 'Draw', 'Game not finished']
+    def entry_check_OK_user(self):
+        while 555 < 666:
+            coords = input('Enter the coordinates:').split(' ')
+            self.row, self.col = coords
+            if not self.col.isnumeric() or not self.row.isnumeric():
+                print('You should enter numbers!')
+            elif not all([x in [str(x) for x in range(1, self.game_size + 1)] for x in [self.col, self.row]]):
+                print(f'Coordinates should be from 1 to {self.game_size}!')
+            elif self.game_progress[self.index_by_coords(int(self.row), int(self.col))] != '_':
+                print('This cell is occupied! Choose another one!')
+            else:
+                break
+        return True
 
-rules = [
-    game_progress[:game_size],
-    game_progress[game_size: game_size * 2],
-    game_progress[game_size * 2: game_size * 3],
+    def make_move(self, who, mark):
+        if who == 'user':
+            if self.entry_check_OK_user():
+                move_cell = self.index_by_coords(int(self.row), int(self.col))
+                self.game_progress[move_cell] = mark
 
-        game_progress[::game_size],
-        game_progress[1::game_size],
-        game_progress[2::game_size],
+    def main(self):
+        self.get_board()
+        self.make_move('user', 'X')
+        self.get_board()
 
-            game_progress[::game_size + 1],
-            game_progress[game_size - 1: pow(game_size, 2) - game_size + 1:
-            game_size - 1]
-    ]
-       
-       
-print(header)
-for i in range(0, pow(game_size, 2), game_size):
-    print('|', *game_progress[i: i + game_size], '|')
-print(footer)
-                                  
-if winnerX in rules and winnerO in rules and abs(game_progress.count('X') - game_progress.count('O')) > 0 \
-    or \
-abs(game_progress.count('X') - game_progress.count('O')) > 1:
-    print(ends_of_game[0])
-elif winnerX in rules:
-    print(ends_of_game[1])
-elif winnerO in rules:
-    print(ends_of_game[2])
-elif '_' not in game_progress:
-    print(ends_of_game[3])
-elif '_' in game_progress:
-    print(ends_of_game[4])
-
-
+BWS_game = TicTacToe()
+BWS_game.main()
